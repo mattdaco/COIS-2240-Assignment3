c9fee77113ab99a5ed9dc973717946ccec36ce42
mattdaco <matthewdaco27@gmail.com>
2025-11-28 20:12:14 -0500

    Refractor RentalSystem class
    Refactored RentalSystem class to include Singleton design pattern.

--- RentalSystem.java
+++ RentalSystem.java
@@ -3,6 +3,18 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class RentalSystem {
+	
+	private static RentalSystem instance;
+	
+	private RentalSystem() {
+	}
+	
+	public static RentalSystem getInstance() {
+        if (instance == null) {
+            instance = new RentalSystem();
+        }
+        return instance;
+    }
     private List<Vehicle> vehicles = new ArrayList<>();
     private List<Customer> customers = new ArrayList<>();
     private RentalHistory rentalHistory = new RentalHistory();
