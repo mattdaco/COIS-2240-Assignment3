18e8c40dafd32d35142382fd1d39aa749deab7e9
mattdaco <matthewdaco27@gmail.com>
2025-11-30 12:13:42 -0500

    capitalize method
    private helper method that formats make and model to first letter uppercase.

--- Vehicle.java
+++ Vehicle.java
@@ -8,19 +8,26 @@ public abstract class Vehicle {
     public enum VehicleStatus { Available, Held, Rented, UnderMaintenance, OutOfService }
 
     public Vehicle(String make, String model, int year) {
-    	if (make == null || make.isEmpty())
-    		this.make = null;
-    	else
-    		this.make = make.substring(0, 1).toUpperCase() + make.substring(1).toLowerCase();
-    	
-    	if (model == null || model.isEmpty())
-    		this.model = null;
-    	else
-    		this.model = model.substring(0, 1).toUpperCase() + model.substring(1).toLowerCase();
-    	
+    	this.make = capitalize(make);
+    	this.model = capitalize(model);
         this.year = year;
         this.status = VehicleStatus.Available;
         this.licensePlate = null;
+    }
+
+    private String capitalize(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            return "Unknown";
+        }
+
+        input = input.trim();
+        if (input.length() == 1) {
+            return input.toUpperCase();
+        }
+
+        String first = input.substring(0, 1).toUpperCase();
+        String rest  = input.substring(1).toLowerCase();
+        return first + rest;
     }
 
     public Vehicle() {
